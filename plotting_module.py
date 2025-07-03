0ec3d589b32ffaa3867d0d045fea18a0dbb7b9d7
Monika Von Cyga <mvoncyga@ucar.edu>
2025-07-03 14:17:14 -0600

    Model/observational difference plots added

--- plotting_module.py
+++ plotting_module.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs               
 import cartopy.feature as cfeature       
 
-def plotting_ozone(data_name, month_index, title, calendar_month, years):
+def plotting_ozone(data_name, month_index, title, calendar_month, years, bar_min, bar_max, supplemental_info=False):
 
     plt.figure(figsize=(10, 6))
     ax = plt.axes(projection=ccrs.PlateCarree())
-    data_name[month_index].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=5, vmax=35, extend='both')
+    data_name[month_index].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=bar_min, vmax=bar_max, extend='both')
     ax.add_feature(cfeature.COASTLINE, linestyle='--')
     ax.set_global()
 
@@ -14,10 +14,11 @@ def plotting_ozone(data_name, month_index, title, calendar_month, years):
     ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
 
     ax.set_title(title + ' ' + calendar_month + ', 2005-2024', fontsize=14)
-    formatted_mean = f"{data_name[month_index].mean().data:.3f}"
 
-    ax.text(0.10, -0.25, 'Mean: ' + str(formatted_mean) + ' DU', va='bottom', ha='center',
-            rotation='horizontal', rotation_mode='anchor',
-            transform=ax.transAxes, fontsize=12)
+    if (supplemental_info==True):
+        formatted_mean = f"{data_name[month_index].mean().data:.3f}"
+        ax.text(0.10, -0.25, 'Mean: ' + str(formatted_mean) + ' DU', va='bottom', ha='center',
+                rotation='horizontal', rotation_mode='anchor',
+                transform=ax.transAxes, fontsize=12)
 
     plt.show()
