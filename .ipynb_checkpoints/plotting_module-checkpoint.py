2ca48acfc34ca0f4ac97bfb6064a65df826059df
Monika Von Cyga <mvoncyga@ucar.edu>
2025-07-03 13:50:16 -0600

    Fixed plotting, CF compliance, date ranges for observational set, and regridded

---
+++ .ipynb_checkpoints/plotting_module-checkpoint.py
@@ -0,0 +1,19 @@
+def plotting_ozone(data_name, month_number, title, calendar_month, years):
+
+    plt.figure(figsize=(10, 6))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    data_name[month].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=5, vmax=35, extend='both')
+    ax.add_feature(cfeature.COASTLINE, linestyle='--')
+    ax.set_global()
+
+    # adding gridlines
+    ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
+
+    ax.set_title(title + ', 2005-2024', fontsize=14)
+    formatted_mean = f"{data_name[month].mean().data:.3f}"
+
+    ax.text(0.10, -0.25, 'Mean: ' + str(formatted_mean) + ' DU', va='bottom', ha='center',
+            rotation='horizontal', rotation_mode='anchor',
+            transform=ax.transAxes, fontsize=12)
+
+    plt.show()
