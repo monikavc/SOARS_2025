a6397e681679532af406db26c50d541c1911fb45
Monika Von Cyga <mvoncyga@ucar.edu>
2025-07-16 15:34:57 -0600

    Lat plot re-evaluated, conversions updated with 240 months

--- plotting_module.py
+++ plotting_module.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs               
 import cartopy.feature as cfeature       
+import imageio
+import os
 
-def plotting_ozone(data_name, month_index, title, calendar_month, years, bar_min, bar_max, supplemental_info=False):
+
+def plotting_ozone(data_name, month_index, title, calendar_month, bar_min, bar_max, color, supplemental_info=False):
 
     plt.figure(figsize=(10, 6))
     ax = plt.axes(projection=ccrs.PlateCarree())
-    data_name[month_index].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=bar_min, vmax=bar_max, extend='both')
+    data_name[month_index].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=bar_min, vmax=bar_max, extend='both', cmap=color)
     ax.add_feature(cfeature.COASTLINE, linestyle='--')
     ax.set_global()
 
     # adding gridlines
-    ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
+    gl = ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
+    gl.right_labels = False
 
-    ax.set_title(title + ' ' + calendar_month + ', 2005-2024', fontsize=14)
+    # set title
+    ax.set_title(title + ' ' + str(calendar_month[month_index]) + ', 2005-2024', fontsize=14)
 
     if (supplemental_info==True):
         formatted_mean = f"{data_name[month_index].mean().data:.3f}"
@@ -22,3 +27,39 @@ def plotting_ozone(data_name, month_index, title, calendar_month, years, bar_min
                 transform=ax.transAxes, fontsize=12)
 
     plt.show()
+
+
+def plotting_ozone_contourf(lon, lat, data_name, month_index, title, calendar_month, bar_min, bar_max, supplemental_info=False):
+
+    plt.figure(figsize=(10, 6))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+
+    cf = plt.contourf(lon,lat,data_name[month_index],transform=ccrs.PlateCarree(central_longitude=0), cmap='rainbow', extend='both', vmin=bar_min, vmax=bar_max)
+    ax.add_feature(cfeature.COASTLINE, linestyle='--')
+    ax.set_global()
+
+    # adding gridlines
+    gl = ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
+    gl.right_labels = False
+
+    # set title
+    ax.set_title(title + ' ' + str(calendar_month[month_index]) + ', 2005-2024', fontsize=14)
+
+    if (supplemental_info==True):
+        formatted_mean = f"{data_name[month_index].mean().data:.3f}"
+        ax.text(0.10, -0.25, 'Mean: ' + str(formatted_mean) + ' DU', va='bottom', ha='center',
+                rotation='horizontal', rotation_mode='anchor',
+                transform=ax.transAxes, fontsize=12)
+    plt.colorbar(cf, shrink=0.8, pad=0.07)
+
+    plt.show()
+
+
+def create_gif(image_folder, output_gif, duration, filename):
+    filenames = sorted([f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))])
+    images = []
+    for filename in filenames:
+        if filename.startswith(filename):
+            image_path = os.path.join(image_folder, filename)
+            images.append(imageio.imread(image_path))
+    imageio.mimsave(output_gif, images, duration=duration)
