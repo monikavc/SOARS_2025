d7ad4af2193a7bd780784ca8461e178e0af8dfa9
Monika Von Cyga <mvoncyga@ucar.edu>
2025-08-07 09:21:39 -0600

    Updated README file

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ Contains all necessary files and scripts.
 
 Directories:
 
-input: Location for direct input from NASA webpage for OMI/MLS ground-to-300hPa and ground-to-100hPa TCO. Model output not included because too large for GitHub storage
-scripts: Contains all code for OMI/MLS climatology, unit converting and model/measurement matching, and plots
-data: Self-made netCDF files for easy use in scripts. {
+1. input: Location for direct input from NASA webpage for OMI/MLS ground-to-300hPa and ground-to-100hPa TCO. Model output not included because too large for GitHub storage
+
+2. scripts: Contains all code for OMI/MLS climatology, unit converting and model/measurement matching, and plots
+
+3. data: Self-made netCDF files for easy use in scripts. {
     datasets_full: Used in time series and certain maps, all latitudes/longitudes of available data are stored here
-    OMI_MLS_climatology_files: Used in zonal averages. Also contains truncated netCDF files used in DOI
+    OMI_MLS_climatology_files: Used in zonal averages. Also contains truncated (75°S-75°N) netCDF files used in DOI
 }
-figures: All relevant figures here
-utils: Self-made modules used in plotting. Not necessary in ADF but can be found helpful
-temp: Temporary files to be deleted. Not necessary in ADF
+
+4. figures: All relevant figures here
+
+5. utils: Self-made modules used in plotting. Not necessary in ADF but can be found helpful
+
+6. temp: Temporary files to be deleted. Not necessary in ADF
--- utils/plotting_module.py
+++ utils/plotting_module.py
@@ -1,24 +1,48 @@
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs               
-import cartopy.feature as cfeature       
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import imageio
 import os
 
 
 def plotting_ozone(data_name, month_index, title, calendar_month, bar_min, bar_max, color, supplemental_info=False):
-
+    """Function used when plotting monthly ozone easily
+    
+        Parameters
+        ----------
+        data_name : str
+            Xarray DataArray you would like to plot, given it is multidimensional
+        month_index : int
+            Index for which you're plotting data_name
+        title : str
+            Title of the plot
+        calendar_month : str
+            Name of the month you're plotting for            
+        bar_min : int
+            Colorbar min value
+        bar_max : int
+            Colorbar max value
+        color : str
+            Matplotlib cmap color value
+        supplemental_info : boolean, optional
+            If the argument `supplemental_info` isn't passed in, no extra mean information will be added
+            Default: no info added
+        """
     plt.figure(figsize=(10, 6))
     ax = plt.axes(projection=ccrs.PlateCarree())
     data_name[month_index].plot(ax=ax,transform=ccrs.PlateCarree(central_longitude=0), vmin=bar_min, vmax=bar_max, extend='both', cmap=color)
     ax.add_feature(cfeature.COASTLINE, linestyle='--')
     ax.set_global()
+    ax.set_facecolor('gray')
 
     # adding gridlines
     gl = ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
     gl.right_labels = False
 
     # set title
-    ax.set_title(title + ' ' + str(calendar_month[month_index]) + ', 2005-2024', fontsize=14)
+    ax.set_title(f'{calendar_month[i]}', fontsize=18)
+    plt.suptitle(title, 
+                 fontsize=28, ha='right', x=0.2, fontweight='bold')
 
     if (supplemental_info==True):
         formatted_mean = f"{data_name[month_index].mean().data:.3f}"
@@ -27,38 +51,24 @@ def plotting_ozone(data_name, month_index, title, calendar_month, bar_min, bar_m
                 transform=ax.transAxes, fontsize=12)
 
     plt.show()
-
-
-def plotting_ozone_contourf(lon, lat, data_name, month_index, title, calendar_month, bar_min, bar_max, supplemental_info=False):
-
-    plt.figure(figsize=(10, 6))
-    ax = plt.axes(projection=ccrs.PlateCarree())
-
-    cf = plt.contourf(lon,lat,data_name[month_index],transform=ccrs.PlateCarree(central_longitude=0), cmap='rainbow', extend='both', vmin=bar_min, vmax=bar_max)
-    ax.add_feature(cfeature.COASTLINE, linestyle='--')
-    ax.set_global()
-
-    # adding gridlines
-    gl = ax.gridlines(draw_labels=True, color='black', alpha=0.5, linestyle='--')
-    gl.right_labels = False
-
-    # set title
-    ax.set_title(title + ' ' + str(calendar_month[month_index]) + ', 2005-2024', fontsize=14)
-
-    if (supplemental_info==True):
-        formatted_mean = f"{data_name[month_index].mean().data:.3f}"
-        ax.text(0.10, -0.25, 'Mean: ' + str(formatted_mean) + ' DU', va='bottom', ha='center',
-                rotation='horizontal', rotation_mode='anchor',
-                transform=ax.transAxes, fontsize=12)
-    plt.colorbar(cf, shrink=0.8, pad=0.07)
-
-    plt.show()
-
 
 def create_gif(image_folder, output_gif, duration, filename):
+    """Function used when creating a gif of ozone maps
+        Parameters
+        ----------
+        image_folder : str
+            Path to where the PNGs you saved are
+        output_gif : str
+            Name of gif you would like to create
+        duration : int
+            How long every figure will be on the screen for, increments of 0.01 seconds
+        filename : str
+            What the PNGs you want to turn into a GIF start with
+        """"
     filenames = sorted([f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))])
     images = []
     for filename in filenames:
+        # include those files that start with certain characters (alternatively use .endswith(filename))
         if filename.startswith(filename):
             image_path = os.path.join(image_folder, filename)
             images.append(imageio.imread(image_path))
